292ab2753c6ced7df2351756bf36c7179e375491
EdricLai <edric.lai25@ycdsbk12.ca>
2024-02-17 00:14:45 -0500

    Updatedgit config user.name EdricLaigit config user.name EdricLaigit config user.name EdricLaigit config user.name EdricLaigit config user.name EdricLaigit config user.name EdricLaigit config user.name EdricLai!

--- Program1.java
+++ Program1.java
@@ -2,12 +2,12 @@ class Program1 extends ConsoleProgram {
 
   /**
   * Description
-  * @author:
+  * @author: Edric
   */
   
   public void run() {
     // your code goes here
-    
+    System.out.println("Hello, I hope this works.");
     
   }
 }
